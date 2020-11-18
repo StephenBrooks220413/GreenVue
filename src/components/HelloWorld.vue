a8ee560b523d8d765d0b459a2f640bbca10d1c07
StephenBrooks220413 <stephen.brooks220413@gmail.com>
2020-11-18 09:28:15 -0600

    updated home page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,134 +1,48 @@
 <template>
   <div class="hello">
-    <!-- top content -->
-    <!-- fluid image and text -->
-    <div class="hello-content">
-      <h1>Mission: Never Alone</h1>
-      <h2>
-        Dedicated to those who served <br>
-        and bringing back each other
-      </h2>
-    </div>
-    <br><br><br><br><br><br><br>
-    <div class="container text-center">
-      <h1> Moving Forward</h1>
-      <br><br>
-      <p>
-        This site is strictly for veterans and family members that want to reach out 
-        to other vetrans or find information on resources available. Sometimes it can be 
-        hard to find what you need and locate where it is, so it is part of the mission to 
-        do that and collaborate with other vets. Whether it is to attend an event, post 
-        something that is going to happen, read the news, it will be readily available and 
-        more as time goes on. As we receive suggestions the site will continously improve. 
-        It is a growing online community with room to grow, and made with flexible technology 
-        to help with expansion for all the vets that want to join.
-      </p>
-    </div>
-    <br><br><br><br><br><br><br>
-    <div class="img-fluid text-center">
-        <img src="https://images.unsplash.com/photo-1568759746401-4af2a427d019?ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80" width="90%" alt="">
+    <div class="jumbotron hello-content"> 
+      <h1 class="text-center display-4">Welcome Home</h1>
     </div>
     <br><br><br><br>
-    <div class="jumbotron text-center">
-      <h1 class="card-header">For Veterans That Stoodby the Wayside</h1>
-      <br>
-      <h4 class="card-text color-primary">
-        Never Forgotten, Never Forsaken
-      </h4>
-      <br><br>
-       <center>
-      <div class="container">
+    <!--about section-->
+    <div class="container text-center">
+      <article itemid="article-section">
+        <h5>
+          A place for vets to find resources easier, wihtout going through hoops and events happening. 
+          Think of it as reading a newspaper but updated weekly on the web. What inspired this was that 
+          I had trouble locating info on benefits, the complicated VA site, nad when there are people 
+          who get paid to help it doesn't seem like they are helping or even care. It is my focus to 
+          provide as much as I can for vets at no cost to them. This is nonprofit and any charity goes 
+          toward veteran programs.
+        </h5>
+      </article>
+    </div>
+    <br><br><br><br><br><br><br>
+    <!-- about author -->
+    <img src="http://lickei.com/Nesdvva/wp-content/uploads/2012/01/namwall.jpg" width="300px" height="300px" alt="">
+      <br><br><br>
+    <div class="container text-center" itemscope itemprop="author">
+      
       <div class="row">
         <div class="col-md-4">
-        <h1>US ARMY</h1>
-          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="https://images.unsplash.com/photo-1569242840838-2a6bdd402fe4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" class="d-block w-75" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="https://images.unsplash.com/photo-1545915454-28aafe3530bf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="d-block w-75" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="https://images.unsplash.com/photo-1511989130945-c2b632959395?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="d-block w-75" alt="...">
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
+        <h1>Experience</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ipsum 
+        rerum totam, porro recusandae quidem voluptatibus 
+        optio non neque sint maiores expedita dicta qui iusto maxime quod cum quasi quam.
         </div>
         <div class="col-md-4">
-        <h1>US NAVY</h1>
-          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="https://images.unsplash.com/photo-1508530786855-dfea35260b8d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" class="d-block w-75" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="https://images.unsplash.com/photo-1509321954256-e03640ea1860?ixlib=rb-1.2.1&auto=format&fit=crop&w=1353&q=80" class="d-block w-75" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="https://images.unsplash.com/photo-1521041601476-47a88cecb403?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" class="d-block w-75" alt="...">
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
+        <h1>Education</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ipsum 
+        rerum totam, porro recusandae quidem voluptatibus 
+        optio non neque sint maiores expedita dicta qui iusto maxime quod cum quasi quam.
         </div>
         <div class="col-md-4">
-        <h1>US AIRFORCE</h1>
-          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="https://images.unsplash.com/photo-1497393627783-2c23c61032bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" class="d-block w-75" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="https://images.unsplash.com/photo-1535329974905-5de1dd801423?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" class="d-block w-75" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="https://images.unsplash.com/photo-1600437096259-bb512849badb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=983&q=80" class="d-block w-75" alt="...">
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
+        <h1>Military Service</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ipsum 
+        rerum totam, porro recusandae quidem voluptatibus 
+        optio non neque sint maiores expedita dicta qui iusto maxime quod cum quasi quam.
         </div>
       </div>
-    </div>
-    </center>
     </div>
   </div>
 </template>
@@ -150,7 +64,7 @@ export default {
   margin-right:5px;
 }
 .hello-content {
-  background: url(https://images.unsplash.com/photo-1511989130945-c2b632959395?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80) no-repeat center center fixed; 
+  background: url(https://www.blogs.va.gov/VAntage/wp-content/uploads/2018/03/Wall-2a.jpg) no-repeat center center fixed; 
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
@@ -158,18 +72,15 @@ export default {
   height: auto;
   padding-top:7%;
   padding-bottom:10%;
-  padding-left:10%;
 }
 .hello-content h1 {
   font-size:3em!important;
   font-weight:600!important;
-  color: rgb(155, 148, 148)!important;
+  color: rgb(149, 155, 148)!important;
   text-shadow: 9px 11px 9px grey!important;
 }
-.hello-content h2 {
-  font-size:2.5em!important;
-  font-weight:500!important;
-  color:teal;
+.hello-content h3 {
+  color:#da9c0d;
 }
 @media (maxwidth:960px){
   .hello-content h1 {
